0551b8959a5e60bef6d563148653997f2fba2398
Siphosenkosi <siphosenkosi17@outlook.com>
2026-02-10 08:08:22 +0200

    feat: add AppDbContext with Bookings and Rooms DbSets

---
+++ ConferenceRoomBooking.Persistence/AppDbContext.cs
@@ -0,0 +1,14 @@
+/*using ConferenceRoomBooking.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConferenceRoomBooking.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Booking> Bookings { get; set; } = null!;
+        public DbSet<ConferenceRoom> Rooms { get; set; } = null!;
+        
+    }
+}*/
